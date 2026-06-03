b8dfc1ce398fff8ff58add7a8efe5b67e3079123
compkari <40903175+compkari@users.noreply.github.com>
2026-06-03 13:39:27 -0700

    Update finAnalysis.py
    checking added code

--- finAnalysis.py
+++ finAnalysis.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import streamlit as st   # add at top with other imports, the rest at the bottom
+import numpy as np
 
 def fetch_data(tickers, start_date, end_date):
     data_frames = []
@@ -23,6 +24,8 @@ def calculate_sortino_ratio(returns, risk_free_rate=0.02):
     downside_deviation = downside_returns.std().mean()
     return (portfolio_return - risk_free_rate) / downside_deviation
 
+def portfolio_optimization(returns):
+    return np.ones(len(returns.columns)) / len(returns.columns)
 
 st.title("Financial Analysis Web App")
 
@@ -43,10 +46,13 @@ st.line_chart(stock_data)
 st.subheader("Stock Returns")
 st.line_chart(returns_data)
 
+
 sharpe_ratio = calculate_sharpe_ratio(returns_data)
 sortino_ratio = calculate_sortino_ratio(returns_data)
 
 st.subheader("Performance Metrics")
 st.write(f"Sharpe Ratio: {sharpe_ratio:.2f}")
 st.write(f"Sortino Ratio: {sortino_ratio:.2f}")
+
+
 
